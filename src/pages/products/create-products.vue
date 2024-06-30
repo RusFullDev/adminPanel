90a194906a21c22c0de81bc43dfb5d6a96a2d8d9
RusFullDev <urinrust@gmail.com>
2024-07-01 00:46:28 +0500

    create product added

--- src/pages/products/create-products.vue
+++ src/pages/products/create-products.vue
@@ -2,54 +2,74 @@
 <template>
     <h1 class="mb-5 text-2xl font-semibold font-serif">Product create</h1>
 
-    <el-form :model="form" label-width="auto" style="max-width: 100%">
-      <el-form-item label="Product title">
+    <el-form :model="form" label-width="auto" style="max-width: 100%" :rules="rules"  ref="ruleForm" >
+      <el-form-item label="Product title" prop="title" >
         <el-input v-model="form.title" />
       </el-form-item>
-      <el-form-item label="Product description">
+      <el-form-item label="Product description" prop="description">
         <el-input v-model="form.description" />
       </el-form-item>
 
-      <el-form-item label="Product price">
+      <el-form-item label="Product price" prop="price">
         <el-input v-model="form.price" />
       </el-form-item>
 
-      <el-form-item label="Product image url">
-        <el-input v-model="form.imgUrl" />
+      <el-form-item label="Product image url" prop="image">
+        <el-input v-model="form.image" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button>Cancel</el-button>
+        <el-button type="primary" @click="submitForm(ruleForm)">Create</el-button>
+        <el-button @click="resetForm(ruleForm)">Cancel</el-button>
       </el-form-item>
     </el-form>
   </template>
   
   <script  setup>
-  import { reactive } from 'vue'
+  import { reactive,ref } from 'vue'
   import axios from 'axios';
+  import {useRouter} from "vue-router"
 
-  
+  const router = useRouter()
+
+  const ruleForm = ref(null)
   const form = reactive({
     title: '',
     description: '',
     price: '',
-    imgUrl: '',
+    image: '',
   })
-  
- 
-  const onSubmit = async () => {
+  const rules = reactive({
+    title:[{ required: true, message: 'Please input title ', trigger: 'change' },],
+  description:[ { required: true, message: 'Please input description ', trigger: 'change' },],
+  price:[  { required: true, message: 'Please input price ', trigger: 'blur' },],
+  image:[  { required: true, message: 'Please input image', trigger: 'blur' },] 
+    
+  })
+
+
+
+
+  const submitForm = async (formEl) => {
   try {
-    const res = await axios.post("https://6681875e04acc3545a06eecd.mockapi.io/products", {
-        title: title.value,
-        description: description.value,
-        price: price.value,
-        imgUrl: imgUrl.value
-    });
+    if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+  axios.post("https://6681875e04acc3545a06eecd.mockapi.io/products",form )
     router.push('/products');
+}
+    })
   } catch (err) {
+    ElNotification.error("error submit!")
     console.log('errors:', err);
+    console.log('error submit!', fields)
   } 
+
 };
+
+const resetForm = (formEl) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
 
   </script>
   
